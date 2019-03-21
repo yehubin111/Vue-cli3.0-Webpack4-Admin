a1be07b231825ff6bb6cd0e16ed073297cc707fe
yehubin <542084894@qq.com>
2019-03-21 14:32:18 +0800

    edit

--- src/atom/components/manage/manage-over.vue
+++ src/atom/components/manage/manage-over.vue
@@ -5,7 +5,7 @@
   width="30%"
   @close="toCancel">
   <span>归档后将不可进入，确认归档？</span>
-  <el-checkbox class="checkbox" v-model="checked">停用广告</el-checkbox>
+  <el-checkbox class="checkbox" v-model="checked">停用广告系列</el-checkbox>
   <span slot="footer" class="dialog-footer">
     <el-button @click="dialogVisible = false">取 消</el-button>
     <el-button type="primary" @click="toOver">确 定</el-button>

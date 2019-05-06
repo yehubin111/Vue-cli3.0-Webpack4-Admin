d2c0dc0aabbb3fda45b8b6542fd0098437ebf439
yehubin <542084894@qq.com>
2019-05-06 17:16:16 +0800

    edit

--- src/atom/store/mutations.js
+++ src/atom/store/mutations.js
@@ -2046,10 +2046,10 @@ export default {
     },
     // template
     TEMPSIZE(state, r) {
-        state.tempsize = Object.keys(r.data).map(v => { return { name: v, delete: r.data[v] } });
+        state.tempsize = r.data ? Object.keys(r.data).map(v => { return { name: v, delete: r.data[v] } }) : [];
     },
     TEMPTRADE(state, r) {
-        state.temptrade = Object.keys(r.data).map(v => { return { name: v, delete: r.data[v] } });
+        state.temptrade = r.data ? Object.keys(r.data).map(v => { return { name: v, delete: r.data[v] } }) : [];
     },
     TEMPLATELIST(state, r) {
         state.templatetotal = r.data.total;
